1d69702826121dea1472ea6773b6d4941fc4d1c0
Chris Martin <chris@cmartindev.com>
2022-04-28 14:42:30 -0700

    Added Get-DiskSpaceInfo.ps1, pending tests

--- src/module/public/Get-DiskSpaceInfo.ps1
+++ src/module/public/Get-DiskSpaceInfo.ps1
@@ -1,4 +1,48 @@
-function Invoke-Main {
+[cmdletbinding()]
+param(
+    [Parameter(
+        Mandatory
+    )]
+    [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
+    [String] $ConfigFilePath
+)
+
+function New-SSHSession {
+    [cmdletbinding()]
+    param(
+        # Input value description
+        [Parameter(
+            Mandatory
+        )]
+        [string] $KeyFilePath,
+
+        [Parameter(
+            Mandatory
+        )]
+        [string] $Username,
+
+        [Parameter(
+            Mandatory,
+            ValueFromPipelineByPropertyName
+        )]
+        [String[]] $Computer
+    )
+
+    process {
+        try {
+            foreach ($computerName in $Computer) {
+                Write-Verbose "Connecting to $computerName"
+
+                New-PSSession -HostName $computerName -KeyFilePath $KeyFilePath -UserName $Username -ErrorAction Stop
+
+            }
+        } catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+}
+
+function Out-PrettyDiskInfo {
     <#
     .DESCRIPTION
 
@@ -12,13 +56,19 @@ function Invoke-Main {
             Mandatory,
             ValueFromPipelineByPropertyName
         )]
-        [String[]] $Path
+        [PSObject[]] $DiskInfo
     )
 
     process {
         try {
-            foreach ($fileName in $Path) {
-                Write-Verbose "Processing [$fileName]"
+            foreach ($disk in $DiskInfo) {
+                Write-Verbose "Processing [$disk]"
+
+                if ($disk.SizeInGB -lt ($disk.TotalSize / 100) * 90) {
+                    "$($PSStyle.Background.BrightRed)Size in GB: $($disk.SizeInGB)$($PSStyle.Reset)"
+                } else {
+                    "$($PSStyle.Background.Green)Size in GB: $($disk.SizeInGB)$($PSStyle.Reset)"
+                }
 
             }
         } catch {
@@ -26,3 +76,19 @@ function Invoke-Main {
         }
     }
 }
+
+$configObject = Get-Content -Path $ConfigFilePath | ConvertFrom-Json
+
+$configObject.Hosts |
+
+    ForEach-Object -Process {
+        New-SSHSession -KeyFilePath $configObject.KeyFilePath -Username $configObject.Username -Computer $_
+    } |
+
+    ForEach-Object -Process {
+        Invoke-Command -Session $_ -ScriptBlock { Get-Disk }
+    } |
+
+    ForEach-Object -Process {
+        Out-PrettyDiskInfo -DiskInfo $_
+    }
