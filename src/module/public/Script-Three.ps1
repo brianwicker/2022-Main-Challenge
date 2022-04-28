edd186ce30afdb57b5295ff276da6e7d600f9657
gschram64 <gschram64@gmail.com>
2022-04-28 18:04:30 -0400

    Update Script-Three.ps1

--- src/module/public/Script-Three.ps1
+++ src/module/public/Script-Three.ps1
@@ -1,9 +1,10 @@
-function Invoke-Main {
+function Get-PSRemotingUsers {
     <#
     .DESCRIPTION
+    show remote connected users using PowerShell remoting. Include both traditional connections and ssh.The output should show the user name, when they connected, how long connected, and if possible their source IP address
 
     .EXAMPLE
-
+    Get-PSRemotingUsers -ComputerName 'YourPC1'
     #>
     [cmdletbinding()]
     param(
@@ -11,16 +12,23 @@ function Invoke-Main {
         [Parameter(
             Mandatory,
             ValueFromPipelineByPropertyName
-        )]
-        [String[]] $Path
+        )]$ComputerName = $env:COMPUTERNAME
     )
 
     process {
         try {
-            foreach ($fileName in $Path) {
-                Write-Verbose "Processing [$fileName]"
-
+            $RemoteInfo = @()
+            foreach ($Computer in $ComputerName) {
+                Write-Verbose "Processing [$ComputerName]"
+                
+                $RemoteInfo.add([PSCustomObject]@{
+                    UserName        = (Get-CimInstance -Class win32_computersystem -ComputerName $ComputerName).UserName
+                    IPAddress       = (Get-CimInstance -Class Win32_NetworkClient -ComputerName $ComputerName).IPAddress
+                    WhenConnected   = (Get-CimInstance -Class Win32_LogonSession -ComputerName $ComputerName).StartTime
+                    LengthConnected = (Get-CimInstance -Class Win32_LogonSession -ComputerName $ComputerName).LengthConnected
+                }]
             }
+            return $RemoteInfo | Format-Table
         } catch {
             $PSCmdlet.ThrowTerminatingError($PSItem)
         }
