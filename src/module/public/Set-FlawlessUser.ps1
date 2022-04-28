26e333ef08bf01493222bc1ddc37a9f26e8ad064
brianwicker <45703606+brianwicker@users.noreply.github.com>
2022-04-28 17:03:31 -0500

    Update Set-FlawlessUser.ps1

--- src/module/public/Set-FlawlessUser.ps1
+++ src/module/public/Set-FlawlessUser.ps1
@@ -5,27 +5,40 @@ function Set-FlawlessUser {
     .Description
         Uses New-PSSession to create / modify a user on a remote system
         SSH Is available as an option
+    .PARAMETER user
+        user to create.  Password will be prompted
+    .PARAMETER computerName
+        ComputerName(s) to create the user on.
+    .PARAMETER connectionUserName
+        User name to use for SSH Connection
+    .PARAMETER connectionKeyPath
+        Path to the key file.
     .EXAMPLE
-        Set-CompanyUser
+        Set-CompanyUser -user Chris -computerName Computer1 -connectionUserName adminUser -connectionKeyPath c:\temp\keyfile
     #>
     [cmdletbinding(DefaultParameterSetName = "default")]
     param(
-        # Input value description
         [Parameter(Mandatory = $True)]
         [System.Management.Automation.PSCredential]
         [System.Management.Automation.Credential()]
         $user,
 
         [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $true
-        )]
-        [String[]] $computerName,
+            ValueFromPipelineByPropertyName = $true)]
+        [String[]]$computerName,
 
         [Parameter(Mandatory = $true)]
         [Switch]$connectionUserName,
 
         [Parameter(Mandatory = $true)]
-        [System.IO.FileInfo]$keyPath,
+        [ValidateScript({
+            if (test-path $_) {
+                $true
+            } else {
+                throw "$_ key file does not exist"
+            }
+        })]
+        [System.IO.FileInfo]$connectionKeyPath,
     )
     Begin{}
     process {
@@ -33,8 +46,8 @@ function Set-FlawlessUser {
                 # using SSH Connection
                 $connection = @{
                     Hostname    = $computer
-                    userName    = $credential.username
-                    keyPath     = $keyPath
+                    userName    = $connectionUsername
+                    keyPath     = $connectionkeyPath
                 }
             # test connection
             Try {
